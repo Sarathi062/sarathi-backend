418b3017eda3442a67a8b591ffe1c3b4ad7fdbdc
Vinayyy <vinaybasargekar@gmail.com>
2024-10-04 07:40:55 +0530

    updated backend (#7)
    * init
    
    * added vercel.json for deployment
    
    * Refactor router and controllers for authentication and event handling
    
    * Refactor router and controllers for authentication and event handling
    
    * Refactor import paths for authentication and profile controllers
    
    * init
    
    * Refactor import paths for authentication and profile controllers
    
    * updated README.md
    
    ---------
    
    Co-authored-by: Sarathi <sarathi062023@gmail.com>

--- models/CreateSession-Model.js
+++ models/CreateSession-Model.js
@@ -1,45 +1,39 @@
 import mongoose from "mongoose";
 
 const CreateSessionSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        required: true
-    },
-    timeFrom: {
-        type: String,
-        required: true
-    },
-    timeTo: {
-        type: String,
-        required: true
-    },
-    
-    price: {
-        type: Number,
-        required: true
-    },
-    mentorID: {
-        type: String,
-        // required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    menteesID: {
-        type: [String],
-        // required: true
-    }
-
-}); 
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	start: {
+		type: Date, // Changed from String to Date for ISO format
+		required: true,
+	},
+	end: {
+		type: Date, // Changed from String to Date for ISO format
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	mentorID: {
+		type: String,
+		// required: true // Uncomment if necessary
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	menteesID: {
+		type: [String],
+		// required: true // Uncomment if necessary
+	},
+});
 
 const CreatedSession = mongoose.model("CreatedSession", CreateSessionSchema);
 
